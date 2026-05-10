992814a458c76dc7a5e14dd5ba1d9421fba04db3
23228cb-colab <23228cb@hvhs.school.nz>
2026-05-11 11:03:50 +1200

    setup

---
+++ fb_setup.js
@@ -0,0 +1,18 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBrHwbyTXvS-zdrQXEIbB7qPjONVLToNVg",
+    authDomain: "sal-s-strawberry-saloon.firebaseapp.com",
+    databaseURL: "https://sal-s-strawberry-saloon-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "sal-s-strawberry-saloon",
+    storageBucket: "sal-s-strawberry-saloon.firebasestorage.app",
+    messagingSenderId: "161645623930",
+    appId: "1:161645623930:web:bbf1727c5340c1d349d9c3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
