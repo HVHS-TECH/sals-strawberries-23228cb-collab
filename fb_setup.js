f575843aae021d0aa02b1bcbfb7d8d63693f163c
23228cb-colab <23228cb@hvhs.school.nz>
2026-05-12 11:46:13 +1200

    set

--- fb_setup.js
+++ fb_setup.js
@@ -1,18 +1,13 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+ // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrHwbyTXvS-zdrQXEIbB7qPjONVLToNVg",
+  authDomain: "sal-s-strawberry-saloon.firebaseapp.com",
+  databaseURL: "https://sal-s-strawberry-saloon-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sal-s-strawberry-saloon",
+  storageBucket: "sal-s-strawberry-saloon.firebasestorage.app",
+  messagingSenderId: "161645623930",
+  appId: "1:161645623930:web:d26be6354d30e40d49d9c3"
+};
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBrHwbyTXvS-zdrQXEIbB7qPjONVLToNVg",
-    authDomain: "sal-s-strawberry-saloon.firebaseapp.com",
-    databaseURL: "https://sal-s-strawberry-saloon-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "sal-s-strawberry-saloon",
-    storageBucket: "sal-s-strawberry-saloon.firebasestorage.app",
-    messagingSenderId: "161645623930",
-    appId: "1:161645623930:web:bbf1727c5340c1d349d9c3"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
