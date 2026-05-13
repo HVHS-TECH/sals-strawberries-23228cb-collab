777f1719ce004f64d6d46080002919fcb21d448e
23228cb-colab <23228cb@hvhs.school.nz>
2026-05-13 15:15:28 +1200

    small

--- script.js
+++ script.js
@@ -5,12 +5,19 @@ function writeForm(){
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
 }
-function UIDWrite() {
+function UIdWrite() {
+  let UID = GLOBAL_user.uid
+  const Name = document.getElementById("name").value
+  const favoriteFruit=document.getElementById("favoriteFruit").value
+   const fruitAmount=document.getElementById("fruitQuantity").value
   console.log("Running UIDWrite")
-  firebase.database().ref('/').once(
-    {
-
-      }
-    )
+  
+  firebase.database().ref('/users/'+UID).set(
+  {  
+    Name:Name,
+    fruit:favoriteFruit,
+    fruitNum:fruitAmount,
+  }
+  )
     }
   
