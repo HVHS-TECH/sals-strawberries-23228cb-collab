9aac41572b06760e5b8876e2a33a42dee03900a5
23228cb-colab <23228cb@hvhs.school.nz>
2026-05-15 09:49:30 +1200

    wread

--- script.js
+++ script.js
@@ -20,4 +20,22 @@ function UIdWrite() {
   }
   )
     }
-  
+  function simpleRead() {
+    let UID = GLOBAL_user.uid
+  console.log("Reading message");
+  firebase.database().ref('/users/'+UID).child('fruit').once('value', display, fb_error);
+  console.log("Leaving simpleRead")
+
+}
+  function display(snapshot) {
+  var dbData = snapshot.val();
+  if (dbData == null) { // if there is no data, dbData will be null.
+  console.log('There was no record when trying to read the message');
+  }
+  else {
+  console.log("The message is: " + dbData)
+  }
+}
+  function fb_error() {
+  console.log(errorMessage)
+}
